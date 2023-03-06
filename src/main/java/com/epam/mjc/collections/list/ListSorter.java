d3a8739a96a5934d3b683da012eef36176da5594
Shodmon-creator <mamayusupovshodmon2003@gmail.com>
2023-03-06 13:47:35 +0500

    Tasks have been completed

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,24 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        ListComparator listComparator = new ListComparator();
+        sourceList.sort(listComparator);
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
-    }
+        int c = Integer.parseInt(a);
+        int d = Integer.parseInt(b);
+            if (Math.abs(c) > Math.abs(d))
+                return 1;
+            else if (Math.abs(c) < Math.abs(d))
+                return -1;
+            else if(c<d)
+                return -1;
+            else return 0;
+        }
+
+
 }
